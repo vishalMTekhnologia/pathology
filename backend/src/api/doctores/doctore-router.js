8fb33579a14638955aa8f4efb98bdafa764068d5
vishalMTekhnologia <vishal.mavalkar@tekhnologiaindia.com>
2026-03-22 10:49:25 +0530

    code

--- backend/src/api/doctores/doctore-router.js
+++ backend/src/api/doctores/doctore-router.js
@@ -6,5 +6,6 @@ export const doctoreRouter = (() => {
 router.post("/add", doctorController.addDoctors);
 router.put("/update", doctorController.updateDoctor);
 router.get("/get-doctore-by-lab-wise", doctorController.getDoctors);
+// hhhhhhhhhhhhhhhhhhh
   return router;
 })();
